3e3c1e2983df1bc014adfc90457b2ad90eb6a5a1
SuprabhSharma <sharmasuprabh2608@gmail.com>
2026-04-16 22:41:14 +0530

    hi

--- run.py
+++ run.py
@@ -2,14 +2,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     host = os.getenv('FLASK_HOST', '0.0.0.0')
-    port = int(os.getenv('PORT', os.getenv('FLASK_PORT', '5000')))
+    port = int(os.getenv('PORT', 7860))
     debug = os.getenv('FLASK_DEBUG', 'true').lower() == 'true'
 
     app.run(host=host, port=port, debug=debug)
